3e5fb2a3c5bf64a85273991204891d4ebb7007ca
Akzel <axelbrandau@hotmail.com>
2019-12-08 19:59:35 +0100

    tptodo

--- todo.js
+++ todo.js
@@ -3,7 +3,7 @@ function createTodo(todoInput) {
         <div class="todo">
             <input type="checkbox" id="checkbox">
             <label for="checkbox"> ${todoInput}</label>
-            <button class="bt-close">&times;</button>
+            <button onclick="closeTodo(this.previousElementSibling.previousElementSibling, this.parentNode)" class="bt-close">&times;</button>
         </div>
     `;
     
@@ -14,12 +14,25 @@ function createTodo(todoInput) {
         return val.length > 2;
     }
 
+    function closeTodo(checkboxTag, todoTag) {
+       if(checkboxTag.checked) {
+        const response = confirm('Es tu sure?');
+        if(!response) return;
+       } document.getElementById('todolist').removeChild(todoTag)
+    }
+
 document.forms.todo.addEventListener(
     'submit',
     function(e) {
         e.preventDefault();
         if(checkInput(this.todoInput.value)){
             createTodo(this.todoInput.value);
+        }else{
+         const errorMsg = document.getElementById('error');
+         errorMsg.style.display = 'block';
+         setTimeout(() => {
+             errorMsg.style.display = 'none';
+         },2500 );
         }
         
     }
